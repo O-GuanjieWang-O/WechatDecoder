805a0cd837f5d9af8d725a0e72dfb0da42d45c7e
O-GuanjieWang-O <1341987719@qq.com>
2023-02-02 21:50:50 +0800

    v1.2

--- move.py
+++ move.py
@@ -4,7 +4,7 @@ import shutil
 
 source_folder =input("Enter source folder: ")
 destination_folder = input("Enter destination folder: ")
-function=int(input("Input the number to perform op:\n 1. move \n 2. copy\n"))
+function=int(input("Input the number to perform op:\n 1. move \n 2. copy\n 3. moveLivp\n 4. moveHeic\n"))
 
 def copy(source,destination_folder):
     for file_name in os.listdir(source):
@@ -32,11 +32,43 @@ def move(source,destination_folder):
          else:
              move(temp,destination_folder)
 
+def moveLivp(source,destination_folder):
+    for file_name in os.listdir(source):
+         if file_name.find("livp") != -1:
+            print(file_name)
+            temp = source +"\\"+file_name
+            print(temp)
+            dest = destination_folder + "\\"+file_name
+            print(dest)
+            if os.path.isfile(temp):
+                shutil.move(temp, dest)
+                print('copyfile:', file_name +" to "+destination_folder)
+            else:
+                move(temp,destination_folder)
+
+def moveHeic(source,destination_folder):
+    for file_name in os.listdir(source):
+         if file_name.find("heic") != -1:
+            print(file_name)
+            temp = source +"\\"+file_name
+            print(temp)
+            dest = destination_folder + "\\"+file_name
+            print(dest)
+            if os.path.isfile(temp):
+                shutil.move(temp, dest)
+                print('copyfile:', file_name +" to "+destination_folder)
+            else:
+                move(temp,destination_folder)
+
 def main():
     if(function==1):
         move(source_folder,destination_folder)
     elif(function==2):
         copy(source_folder,destination_folder)
+    elif(function==3):
+        moveLivp(source_folder,destination_folder)
+    elif(function==4):
+        moveHeic(source_folder,destination_folder)
 
 
 if __name__ == "__main__":
